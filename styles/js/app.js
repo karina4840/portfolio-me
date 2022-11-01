3e6184f443c8c08250a84f41db4c65d57b4e5a76
karina ivanova <86211129+karina4840@users.noreply.github.com>
2022-11-01 18:50:57 +0500

    translation feature + font

--- styles/js/app.js
+++ styles/js/app.js
@@ -3,22 +3,38 @@ const menu = $("#nav-elements-id");
 //=========APPEARING OF THE MENU==========
 
 $(".menu-button").click(() => {
-    menu.toggleClass("shown");
+  menu.toggleClass("shown");
 });
 
 //=========SMOOTH SCROLLING==========
 
-$(document).ready(function(){
-    $('a[href^="#"]').on('click',function (e) {
-        e.preventDefault();
+$(document).ready(function () {
+  $('a[href^="#"]').on("click", function (e) {
+    e.preventDefault();
 
-        var target = this.hash;
-        $target = $(target);
+    var target = this.hash;
+    $target = $(target);
 
-        $('html, body').stop().animate({
-            'scrollTop':  $target.offset().top
-        }, 600, 'swing', function () {
-            window.location.hash = target;
-        });
-    });
+    $("html, body")
+      .stop()
+      .animate(
+        {
+          scrollTop: $target.offset().top,
+        },
+        600,
+        "swing",
+        function () {
+          window.location.hash = target;
+        }
+      );
+  });
+});
+
+//=========LANGUAGE FEATURE==========
+$('[lang="en"]').hide();
+
+$("#switch-lang").click(function (e) {
+  e.preventDefault();
+  $('[lang="ru"]').toggle();
+  $('[lang="en"]').toggle();
 });
